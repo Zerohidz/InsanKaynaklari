c76a110deeef622253e783fa81f7ca0874ba0bd7
Dursun Zahid Korkmaz <dursunzahidkorkmaz@gmail.com>
2023-05-11 23:53:15 +0300

    SingletonPersistent

--- Assets/Scripts/Singletons.cs
+++ Assets/Scripts/Singletons.cs
@@ -1,22 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    public static T Instance;
+    public static T Instance { get; private set; }
 
     protected virtual void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
+        if (Instance != null)
             Destroy(gameObject);
-        }
+        Instance = this as T;
     }
 
     private void OnApplicationQuit()
@@ -26,5 +18,14 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
             Instance = null;
             Destroy(gameObject);
         }
+    }
+}
+
+public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
     }
 }
--- Assets/Scripts/Systems.cs
+++ Assets/Scripts/Systems.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Systems : Singleton<Systems>
+public class Systems : PersistentSingleton<Systems>
 {
 
 }
