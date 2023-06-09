98fd3ccb9331a54e6e2000aa9538dd2d3e37b501
Dursun Zahid Korkmaz <dursunzahidkorkmaz@gmail.com>
2023-06-09 16:22:39 +0300

    Color Palette ScriptableObject

---
+++ Assets/ColorPalette.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewColorPalette", menuName = "Scriptable Objects/Color Palette")]
+public class ColorPalette : ScriptableObject
+{
+    public Color Color0;
+    public Color Color1;
+    public Color Color2;
+    public Color Color3;
+    public Color Color4;
+    public Color Color5;
+}
